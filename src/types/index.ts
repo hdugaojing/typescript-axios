8485eff00167d87cd2c2332baeb7d8ed9c0d706a
Colin <colingaojing@163.com>
2020-03-05 23:57:34 +0800

    feat: 基础请求代码逻辑

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+//字符串字面量类型
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
