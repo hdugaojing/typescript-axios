1a96ae136e4d987bbadcacf422c18330165b70c0
Colin <colingaojing@163.com>
2020-03-08 14:54:06 +0800

    feat: 处理请求headers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
   xhr(config)
@@ -9,6 +10,8 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  // 因为处理 header 的时候依赖了 data，所以要在处理请求 body 数据之前处理请求 header。
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -21,6 +24,12 @@ function transformUrl(config: AxiosRequestConfig): string {
 // 处理请求data
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+// 处理请求headers
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,20 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toLowerCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    // 没有数据可以不设置content-type
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
